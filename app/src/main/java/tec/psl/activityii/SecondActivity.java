f20bbd3e6c3d8ad186b6702c700874b5f2bf516a
psl-tec-dk <psl@tec.dk>
2022-05-05 10:29:23 +0200

    Tilføjet spinner og listview

--- app/src/main/java/tec/psl/activityii/SecondActivity.java
+++ app/src/main/java/tec/psl/activityii/SecondActivity.java
@@ -4,14 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class SecondActivity extends AppCompatActivity {
 
     private Button btn_closeSecond;
     private TextView txt_msgSecond;
     private Intent intent;
+    private Spinner spn_drink, spn_mad;
+    private ArrayList<String> list = new ArrayList<>();
+    private String chosen = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +29,38 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
 
         txt_msgSecond = findViewById(R.id.txt_msgSecond);
+        spn_drink = findViewById(R.id.spn_drink);
+        spn_mad = findViewById(R.id.spn_mad);
+
+        list.add("Vælg fra listen");
+        list.add("The");
+        list.add("Kaffe");
+        list.add("Chokolade");
+        list.add("Cappuchino");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
+        spn_drink.setAdapter(adapter);
+        spn_mad.setAdapter(adapter);
+
+        spn_drink.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long l) {
+                if(pos == 0) { return; }
+//                Toast.makeText(SecondActivity.this, "Pos: " + pos, Toast.LENGTH_SHORT).show();
+                chosen = list.get(pos);
+                Toast.makeText(SecondActivity.this, "Dit Valg: " + list.get(pos), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) { }
+        });
 
         intent = getIntent();
         txt_msgSecond.setText(intent.getStringExtra("fromMain"));
 
         btn_closeSecond = findViewById(R.id.btn_closeSecond);
         btn_closeSecond.setOnClickListener( v-> {
-            intent.putExtra("msg", "Hilsen fra second");
+            intent.putExtra("msg", chosen);
             setResult(AppConstants.RESULT_CODE_SECOND, intent);
             finish();
         });
--- app/src/main/java/tec/psl/activityii/ThirdActivity.java
+++ app/src/main/java/tec/psl/activityii/ThirdActivity.java
@@ -1,23 +1,62 @@
 package tec.psl.activityii;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ThirdActivity extends AppCompatActivity {
 
-    private Button btn_closeThird;
+    private Button btn_closeThird, btn_addItem;
+    private EditText et_newItem;
     private Intent intent;
+    private ListView lst_myList;
+    private ArrayList<String> list;
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putStringArrayList("list", list);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
 
+        btn_addItem = findViewById(R.id.btn_addItem);
+        et_newItem = findViewById(R.id.ed_newItem);
+
         btn_closeThird = findViewById(R.id.btn_closeThird);
+        lst_myList = findViewById(R.id.lst_myList);
+
+        if(savedInstanceState == null) {
+            list =  new ArrayList<>();
+        }
+        else {
+            list = savedInstanceState.getStringArrayList("list");
+        }
+
+//        list =  new ArrayList<>();
+//        list.add("Købe mælk");
+//        list.add("Ringe til Mark");
+//        list.add("Hente pakke på posthus");
+//        list.add("Købe printerpapir");
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+        lst_myList.setAdapter(adapter);
+
         intent = getIntent();
 
         Log.d("PSL_LOG1", intent.getStringExtra("fromMain"));
@@ -26,6 +65,25 @@ public class ThirdActivity extends AppCompatActivity {
             intent.putExtra("msg", "Hilsen fra Third");
             setResult(AppConstants.RESULT_CODE_THIRD, intent);
             finish();
+        });
+
+        btn_addItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(et_newItem.getText().toString().equals("")) return;
+                String newItem = et_newItem.getText().toString();
+                list.add(newItem);
+                adapter.notifyDataSetChanged();
+                et_newItem.setText("");
+            }
+        });
+        lst_myList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int pos, long l) {
+                list.remove(pos);
+                adapter.notifyDataSetChanged();
+                return false;
+            }
         });
 
     }
